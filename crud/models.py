52bd20f889c3f6e144003bc5e64546a04d2512f3
Juanets <jdgs.gt@gmail.com>
2018-03-23 18:21:11 -0600

    Modelos ajustados

--- crud/models.py
+++ crud/models.py
@@ -2,17 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=50)
-    correo = models.CharField(blank=True, max_length=70)
-    telefono = models.CharField(blank=True, max_length=13)
+    nombre = models.CharField(max_length=100)
+    correo = models.CharField(blank=True, max_length=60)
+    telefono = models.CharField(blank=True, max_length=50)
     colonia = models.CharField(max_length=50)
     calle = models.CharField(max_length=50)
-    numero_int = models.CharField(blank=True, max_length=50)
-    numero_ext = models.CharField(blank=True, max_length=50)
-    colonia = models.CharField(max_length=50)
-    lat = models.CharField(blank=True, max_length=20)
-    lng = models.CharField(blank=True, max_length=20)
-    zona = models.CharField(blank=True, max_length=6)
+    numero_int = models.CharField(blank=True, max_length=20)
+    numero_ext = models.CharField(blank=True, max_length=20)
+    lat = models.CharField(max_length=20)
+    lng = models.CharField(max_length=20)
+    zona = models.CharField(max_length=6)
 
 
 class Chofer(models.Model):
