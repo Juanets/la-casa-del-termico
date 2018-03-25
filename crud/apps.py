6ffeeffc6335283b44571a15da59b5f2bf4e9300
Juanets <jdgs.gt@gmail.com>
2018-03-25 01:14:05 -0600

    Config de django-watson

--- crud/apps.py
+++ crud/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
-
+from watson import search as watson
 
 class CrudConfig(AppConfig):
     name = 'crud'
+    def ready(self):
+        Cliente = self.get_model('Cliente')
+        watson.register(Cliente)
