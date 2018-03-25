98da1a2bea1221aa8a085492cd010cf054660769
Juanets <jdgs.gt@gmail.com>
2018-03-25 01:14:30 -0600

    URLs para manejar la busqueda de clientes

--- crud/urls.py
+++ crud/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clientes/', views.clientes_buscar, name='clientes_buscar'),
-    path('clientes/<int:id>', views.clientes_ver, name='clientes_ver'),
+    path('clientes/', views.clientes_lista, name='clientes_lista'),
+    path('clientes/<int:id>/', views.clientes_ver, name='clientes_ver'),
+    path('clientes/buscar_handler/', views.clientes_buscar_handler, name='clientes_buscar_handler'),        
+    path('clientes/buscar/<str:query>', views.clientes_buscar, name='clientes_buscar'),    
 ]
