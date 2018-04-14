630c401754d8c68fc1112661b540a5d21018c659
Juanets <jdgs.gt@gmail.com>
2018-04-14 16:28:14 -0600

    Se agregaron urls de la app rutas

--- termico/urls.py
+++ termico/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('', include('crud.urls'))
+    path('', include('crud.urls')),
+    path('', include('rutas.urls'))    
 ]
