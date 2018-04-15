91b3bc9b010f3368e343750d9f03bf5e4f62f9d0
Juanets <jdgs.gt@gmail.com>
2018-04-14 21:26:57 -0600

    Ajustes para guardar los reportes

--- rutas/models.py
+++ rutas/models.py
@@ -3,9 +3,12 @@ from django.db import models
 # Create your models here.
 class Reporte(models.Model):
     fecha = models.DateField()
-    clientes = models.ManyToManyField('crud.Cliente')
+    clientes = models.ManyToManyField('crud.Cliente', through='DeliveringOrder')
     chofer = models.ForeignKey('crud.Chofer', on_delete=models.CASCADE)
     distancia = models.CharField(max_length=10)
-    duracion_int = models.IntegerField()
-    duracion_str = models.CharField(max_length=30)
-    # warnings[]
+    duracion = models.CharField(max_length=30)
+
+class DeliveringOrder(models.Model):
+    number = models.PositiveIntegerField()
+    r = models.ForeignKey(Reporte, on_delete=models.CASCADE)
+    c = models.ForeignKey('crud.Cliente', on_delete=models.CASCADE)
