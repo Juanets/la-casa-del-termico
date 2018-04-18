7419c2beea0974ea35ebea3eca595df067755029
Juanets <jdgs.gt@gmail.com>
2018-04-18 16:26:09 -0700

    Agregado el campo de URL de ruta

--- rutas/models.py
+++ rutas/models.py
@@ -7,6 +7,8 @@ class Reporte(models.Model):
     chofer = models.ForeignKey('crud.Chofer', on_delete=models.CASCADE)
     distancia = models.CharField(max_length=10)
     duracion = models.CharField(max_length=30)
+    mapa_url = models.CharField(max_length=500)
+
 
 class DeliveringOrder(models.Model):
     number = models.PositiveIntegerField()
