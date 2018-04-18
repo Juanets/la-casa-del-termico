89be95c43edf86ec14a717204effad525b12bd8c
Juanets <jdgs.gt@gmail.com>
2018-04-18 16:27:03 -0700

    URLs para ver reportes en el sistema y PDF

--- rutas/urls.py
+++ rutas/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('rutas/nueva/', views.generar_ruta, name='rutas_nueva'),  
     path('rutas/asdf', views.asdf, name='asdf'),
-    path('rutas/guardar/', views.guardar_ruta, name='rutas_guardar'),    
-    path('reporte-<int:id>.pdf', views.pdf, name='pdf'),    
+    path('rutas/guardar/', views.guardar_ruta, name='rutas_guardar'),
+    path('reportes/<int:id>/', views.reporte_ver, name='reporte_ver'),  
+    path('reportes/Reporte <int:id> - <str:fecha>.pdf', views.reporte_pdf, name='reporte_pdf'),    
 ]
