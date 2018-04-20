523ee44db56601fe27e144ed04da361cb83645ff
Juanets <jdgs.gt@gmail.com>
2018-04-19 22:40:09 -0700

    URLs de listado de reportes, busqueda y eliminacion

--- rutas/urls.py
+++ rutas/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('rutas/nueva/', views.escoger_clientes, name='rutas_nueva'),
     path('rutas/guardar/', views.guardar_ruta, name='rutas_guardar'),
     path('reportes/<int:id>/', views.reporte_ver, name='reporte_ver'),  
-    path('reportes/Reporte <int:id> - <str:fecha>.pdf', views.reporte_pdf, name='reporte_pdf'),    
+    path('reportes/Reporte <int:id> - <str:fecha>.pdf', views.reporte_pdf, name='reporte_pdf'), 
+    path('reportes/buscar_handler/', views.reporte_buscar_handler, name='reporte_buscar_handler'),
+    path('reportes/buscar/<str:query>/', views.reporte_buscar, name='reporte_buscar'),
+    path('reportes/', views.reportes_lista, name='reportes_lista'),
+    path('reportes/borrar/<int:id>/', views.reportes_borrar, name='reportes_borrar'),                      
 ]
