b50c773d32da585287e35aec403b0aa0fe554121
Juanets <jdgs.gt@gmail.com>
2018-04-19 22:41:55 -0700

    Config de watson en apps.py

--- rutas/apps.py
+++ rutas/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
-
+from watson import search as watson
 
 class RutasConfig(AppConfig):
     name = 'rutas'
+    def ready(self):
+        Reporte = self.get_model('Reporte')
+        watson.register(Reporte)
