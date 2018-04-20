67164ff76dcf7efa082e4a55a018d7bd7eb16d73
Juanets <jdgs.gt@gmail.com>
2018-04-19 22:37:54 -0700

    Agregados campos de chofer_nombre y fecha_str

--- rutas/models.py
+++ rutas/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class Reporte(models.Model):
     fecha = models.DateField()
+    fecha_str = models.CharField(max_length=50)
     clientes = models.ManyToManyField('crud.Cliente', through='DeliveringOrder')
     chofer = models.ForeignKey('crud.Chofer', on_delete=models.CASCADE)
+    chofer_nombre = models.CharField(max_length=100)
     distancia = models.CharField(max_length=10)
     duracion = models.CharField(max_length=30)
     mapa_url = models.CharField(max_length=500)
