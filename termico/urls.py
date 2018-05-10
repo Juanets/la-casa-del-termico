79dce2b56328f959fa81d1655a7a292c0458abbc
Juanets <jdgs.gt@gmail.com>
2018-05-10 00:13:01 -0700

    URLs de la API

--- termico/urls.py
+++ termico/urls.py
@@ -15,10 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+
+from rest_framework import routers
+
 from . import views
+
+# API routes
+router = routers.DefaultRouter()
+router.register(r'clientes', views.ClienteViewSet)
+router.register(r'choferes', views.ChoferViewSet)
+router.register(r'reportes', views.ReporteViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', views.home, name='home'),
     path('', include('crud.urls')),
     path('', include('rutas.urls')),
