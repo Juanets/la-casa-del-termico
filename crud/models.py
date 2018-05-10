bc9e132ce83103b72892c82441fe3e16874d4305
Juanets <jdgs.gt@gmail.com>
2018-05-10 00:12:11 -0700

    Ajustes para postgres

--- crud/models.py
+++ crud/models.py
@@ -5,15 +5,15 @@ from django.db import models
 class Cliente(models.Model):
     '''Campos del cliente.'''
     nombre = models.CharField(max_length=100)
-    correo = models.CharField(blank=True, max_length=60)
-    telefono = models.CharField(blank=True, max_length=100)
+    correo = models.EmailField(blank=True)
+    telefono = models.BigIntegerField(blank=True)
     colonia = models.CharField(max_length=100)
     calle = models.CharField(max_length=100)
-    cp = models.CharField(blank=True, max_length=5)
+    cp = models.IntegerField(blank=True)
     numero_int = models.CharField(blank=True, max_length=20)
     numero_ext = models.CharField(blank=True, max_length=20)
-    lat = models.CharField(max_length=20)
-    lng = models.CharField(max_length=20)
+    lat = models.DecimalField(max_digits=25, decimal_places=20)
+    lng = models.DecimalField(max_digits=25, decimal_places=20)
     zona = models.CharField(max_length=6)
 
 
