e2c676aa4a14b61df318f825a4d97a09e00f6acc
Juanets <jdgs.gt@gmail.com>
2018-05-10 00:13:22 -0700

    Views de la API, regresa todos los objetos de clientes, choferes y reportes

--- termico/views.py
+++ termico/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from rest_framework import viewsets
+
+from termico.serializers import ClienteSerializer, ChoferSerializer, ReporteSerializer
+from crud.models import Cliente, Chofer
+from rutas.models import Reporte
+
 @login_required
 def home(request):
     '''Mostrar la landing page, la pagina principal.'''
@@ -10,3 +16,16 @@ def home(request):
 def ayuda(request):
     '''Mostrar pagina de ayuda.'''
     return render(request, 'ayuda.html')
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+
+class ChoferViewSet(viewsets.ModelViewSet):
+    queryset = Chofer.objects.all()
+    serializer_class = ChoferSerializer
+
+class ReporteViewSet(viewsets.ModelViewSet):
+    queryset = Reporte.objects.all()
+    serializer_class = ReporteSerializer
