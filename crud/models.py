5557bf8ba451ecceb69599ff0224cba8358c9375
Juanets <jdgs.gt@gmail.com>
2018-05-12 01:36:55 -0700

    Fix a fields y agregando null values

--- crud/models.py
+++ crud/models.py
@@ -5,13 +5,13 @@ from django.db import models
 class Cliente(models.Model):
     '''Campos del cliente.'''
     nombre = models.CharField(max_length=100)
-    correo = models.EmailField(blank=True)
-    telefono = models.CharField(max_length=50, blank=True)
+    correo = models.EmailField(blank=True, null=True)
+    telefono = models.CharField(max_length=50, blank=True, null=True)
     colonia = models.CharField(max_length=100)
     calle = models.CharField(max_length=100)
-    cp = models.CharField(max_length=10, blank=True)
-    numero_int = models.CharField(blank=True, max_length=20)
-    numero_ext = models.CharField(blank=True, max_length=20)
+    cp = models.IntegerField(blank=True, null=True)
+    numero_int = models.CharField(blank=True, null=True, max_length=20)
+    numero_ext = models.CharField(blank=True, null=True, max_length=20)
     lat = models.DecimalField(max_digits=25, decimal_places=20)
     lng = models.DecimalField(max_digits=25, decimal_places=20)
     zona = models.CharField(max_length=6)
